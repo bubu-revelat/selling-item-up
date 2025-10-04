4f6971cf4e170ee91c1ceb3228e1a2b3c1ae08d8
Bubu <80535543+bubu-revelat@users.noreply.github.com>
2025-10-04 13:02:37 -0400

    Update picture and prices

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const products = [
     "id": 1,
     "name": "Cama cucheta en L. 1 plaza. Marca \"verde limon\"",
     "description": "•⁠  ⁠cama alta + escalera anexada (movil) 2,40 mts\n•⁠  ⁠cama baja 1,90 más de largo (movil) \n•⁠  ⁠ancho base escalera 0.90 más\n•⁠  ⁠ancho camas 0.90 mts",
-    "price": 0,
+    "price": 450000,
     "imageSrcs": [ 
       "/images/cucheta1.jpeg",
       "/images/cucheta2.jpeg",
@@ -15,7 +15,7 @@ export const products = [
     "id": 2,
     "name": "Mueble de guardado fácil",
     "description": "•⁠  ⁠ancho 1.30 mts\n•⁠  ⁠alto 1,60 mts\n•⁠  ⁠profundidad máxima 0,49 mts",
-    "price": 0,
+    "price": 115000,
     "imageSrcs": [
       "/images/mueble1.jpeg",
       "/images/mueble2.jpeg"
@@ -25,7 +25,7 @@ export const products = [
     "id": 3,
     "name": "Estanterías blancas de metal (2 unidades)",
     "description": "•⁠ alto 1.90 mts\n•⁠ ancho 0,90 mts",
-    "price": 0,
+    "price": 68000,
     "imageSrcs": [
       "/images/estantes_metal1.jpeg",
       "/images/estantes_metal2.jpeg"
@@ -35,7 +35,7 @@ export const products = [
     "id": 4,
     "name": "Atril",
     "description": "•⁠ alto 2 mts\n•⁠ ancho 0,60 mts",
-    "price": 0,
+    "price": 25000,
     "imageSrcs": [
       "/images/atril.jpeg",
     ]
@@ -44,7 +44,7 @@ export const products = [
     "id": 5,
     "name": "Mampara baño de aluminio y poliuretano. Blanca con detalle de flor en paño central.",
     "description": "•⁠  ⁠3 paños de 1,40 mts de altura x 0,60 de ancho\n•⁠  ⁠1paño de 1,40 mts alto x 0,76 ancho",
-    "price": 0,
+    "price": 85000,
     "imageSrcs": [
       "/images/mampara.jpeg",
     ]
@@ -53,7 +53,7 @@ export const products = [
     "id": 6,
     "name": "Parrilla con chapones base",
     "description": "•⁠  ⁠parrilla: 1,50 largo x 0,70 ancho x 0,20 altura\n•⁠  ⁠chapones x 3 u.: 0,90 x 0,60",
-    "price": 0,
+    "price": 45000,
     "imageSrcs": [
       "/images/parrilla.jpeg",
     ]
@@ -62,7 +62,7 @@ export const products = [
     "id": 7,
     "name": "Puerta de reja de caño",
     "description": "•⁠ Alto 1 mt\n•⁠ Ancho 0,73 mts (incluido parante con bisagra)",
-    "price": 0,
+    "price": 45000,
     "imageSrcs": [
       "/images/puerta.jpeg",
     ]
@@ -71,7 +71,7 @@ export const products = [
     "id": 8,
     "name": "Biombo de madera y junco ( esterilla) 3 hojas",
     "description": "• Medidas por paño: 1,70 alto x 0.75 ancho",
-    "price": 0,
+    "price": 45000,
     "imageSrcs": [
       "/images/biombo_de_madera.jpeg",
     ]
@@ -90,8 +90,8 @@ export const products = [
   {
     "id": 10,
     "name": "Sillas de Caño varias",
-    "description": "",
-    "price": 0,
+    "description": "2x $25.000",
+    "price": 15000,
     "imageSrcs": [
       "/images/silla.jpeg",
     ]
@@ -100,7 +100,7 @@ export const products = [
     "id": 11,
     "name": "Juego de 2 estantes de melamina color nogal",
     "description": "• Medidas: 0,20 x 0,30",
-    "price": 0,
+    "price": 11000,
     "imageSrcs": [
       "/images/estantes.jpeg",
     ]
@@ -109,7 +109,7 @@ export const products = [
     "id": 12,
     "name": "Juego de estantes de madera maciza con perfiles de hierro",
     "description": "•⁠  ⁠Madera 0,25 x 0,30 mt. espesor 3,5 cm\n•⁠  ⁠perfiles: 0,20 x 0,20 mt",
-    "price": 0,
+    "price": 55000,
     "imageSrcs": [
       "/images/estantes_madera.jpeg",
     ]
@@ -118,7 +118,7 @@ export const products = [
     "id": 13,
     "name": "Espejo de hierro con estantes en madera maciza(hace juego con estantes)",
     "description": "• Espejo: 1,34 mts alto x 1 mt ancho\n• Estante madera: 1 mt largo x 0,19 mt ancho y espesor 3,5 cm",
-    "price": 0,
+    "price": 125000,
     "imageSrcs": [
       "/images/espejo1.jpeg",
       "/images/espejo2.jpeg",
@@ -129,7 +129,7 @@ export const products = [
     "id": 14,
     "name": "Mueble antiguo restaurado. Excelente estado de conservacion",
     "description": "• Altura 1.25 MT\n• Ancho 1,60 mt\n• Prof 0,45",
-    "price": 0,
+    "price": 355000,
     "imageSrcs": [
       "/images/mueble_viejo1.jpeg",
       "/images/mueble_viejo2.jpeg"
@@ -139,7 +139,7 @@ export const products = [
     "id": 15,
     "name": "Perchero",
     "description": "• 32 cm largo",
-    "price": 0,
+    "price": 8000,
     "imageSrcs": [
       "/images/perchero1.jpeg",
     ]
@@ -148,7 +148,7 @@ export const products = [
     "id": 16,
     "name": "Biblioteca blanca laqueada",
     "description": "• Alto: 1,80 mts\n• Ancho: 0,40\n• Profundo x 0,30",
-    "price": 0,
+    "price": 85000,
     "imageSrcs": [
       "/images/biblioteca_laqueada1.jpeg",
     ]
@@ -156,8 +156,8 @@ export const products = [
   {
     "id": 17,
     "name": "Biblioteca de estilo x 2 unidades",
-    "description": "• Alto: 2 mts\n• Ancho: 0,75\n• Profundo x 0,30",
-    "price": 0,
+    "description": "• Alto: 2 mts\n• Ancho: 0,75\n• Profundo x 0,30 /n 2x $20.000",
+    "price": 11000,
     "imageSrcs": [
       "/images/biblio_antigua1.jpeg",
       "/images/biblio_antigua2.jpeg",
@@ -167,7 +167,7 @@ export const products = [
     "id": 18,
     "name": "Juego de sillones tipo chesterfileld",
     "description": "",
-    "price": 0,
+    "price": 450000,
     "imageSrcs": [
       "/images/juego_sillones1.jpeg",
       "/images/juego_sillones2.jpeg",
@@ -178,7 +178,7 @@ export const products = [
     "id": 19,
     "name": "Juego de mesa ratona laqueada en negro + 2 bancos. Escuadra irregular",
     "description": "• Largo 1,15 mts\n• Ancho: 0,50",
-    "price": 0,
+    "price": 85000,
     "imageSrcs": [
       "/images/mesa_ratona1.jpeg",
       "/images/mesa_ratona2.jpeg"
@@ -188,7 +188,7 @@ export const products = [
     "id": 20,
     "name": "Mueble aparador de estilo. Madera maciza",
     "description": "• Largo 1,88 mts\n• Alto: 0,90 \n• Prof: x 0,40",
-    "price": 0,
+    "price": 245000,
     "imageSrcs": [
       "/images/mueble_aparador1.jpeg"
     ]
@@ -197,7 +197,7 @@ export const products = [
     "id": 21,
     "name": "Mueble de estilo antiguo",
     "description": "• Alto 1,90 mts\n• Ancho: 0,50 \n• Prof: x 0,60",
-    "price": 0,
+    "price": 72000,
     "imageSrcs": [
       "/images/repisa_antigua1.jpeg",
       "/images/repisa_antigua2.jpeg"
@@ -207,7 +207,7 @@ export const products = [
     "id": 22,
     "name": "Apliques luz de pared  x 4 unidades",
     "description": "",
-    "price": 0,
+    "price": 35000,
     "imageSrcs": [
       "/images/apliques_chicos1.jpeg"
     ]
@@ -216,7 +216,7 @@ export const products = [
     "id": 23,
     "name": "Apliques luz aluminio",
     "description": "",
-    "price": 0,
+    "price": 55000,
     "imageSrcs": [
       "/images/apliques_aluminio1.jpeg",
       "/images/apliques_aluminio2.jpeg"
@@ -226,27 +226,77 @@ export const products = [
     "id": 24,
     "name": "Araña colgante antigua madera y metal",
     "description": "Diametro: 0,65 cm\n• Largo 3 mts\n• Con cadena",
-    "price": 0,
+    "price": 65000,
     "imageSrcs": [
       "/images/arania1.jpeg"
     ]
   },
   {
     "id": 25,
-    "name": "Niños bien educados",
-    "description": "niña 1.54 mts\n• niño 1.30 mts\n• Colores varios\n• No se venden por separado",
-    "price": 0,
+    "name": "Araña colgante blanca",
+    "description": "Diametro: 50cm",
+    "price": 35000,
     "imageSrcs": [
-      "/images/ninios1.jpeg"
+      "/images/arania_blanca1.jpeg",
+      "/images/arania_blanca2.jpeg"
     ]
   },
   {
     "id": 26,
-    "name": "Capibara de ciudad",
-    "description": "Largo: 100mts\n• Alto x 0.25cms",
+    "name": "Araña colgante madera y vidrio",
+    "description": "Diametro: 50cm",
     "price": 0,
     "imageSrcs": [
-      "/images/capibara1.jpeg"
+      "/images/arania_madera1.jpeg",
+      "/images/arania_madera2.jpeg",
+      "/images/arania_madera3.jpeg",
+      "/images/arania_madera4.jpeg"
+    ]
+  },
+  {
+    "id": 27,
+    "name": "Veladores o apliques de pared madera y vidrio x 2 unidades",
+    "description": "",
+    "price": 0,
+    "imageSrcs": [
+      "/images/aplique_madera1.jpeg"
+    ]
+  },
+  {
+    "id": 28,
+    "name": "Plafon de vidrio esmerilado color blanco. X 2",
+    "description": "Medidas:  0.25 x 0.25 mts /n 2x $20.000",
+    "price": 13000,
+    "imageSrcs": [
+      "/images/plafon_blanco1.jpeg",
+      "/images/plafon_blanco2.jpeg"
+    ]
+  },
+  {
+    "id": 29,
+    "name": "Farol de metal con vidrio x 2 unidades",
+    "description": "Medida: 0.35 mts largo /n 2x $50.000",
+    "price": 28000,
+    "imageSrcs": [
+      "/images/farol_blanco1.jpeg"
+    ]
+  },
+  {
+    "id": 30,
+    "name": "Farol metal colgante",
+    "description": "Medida: 0.35 mt largo + cadena",
+    "price": 20000,
+    "imageSrcs": [
+      "/images/farol_metal_colgante1.jpeg"
+    ]
+  },
+  {
+    "id": 30,
+    "name": "Monopatin cobra 720 con luces led plegable",
+    "description": "Led, plegable",
+    "price": 95000,
+    "imageSrcs": [
+      "/images/monopatin1.jpeg"
     ]
   }
 ]
